64c1cc8a646186a5d9d5f6a74fb5de529ae0f6c3
MilesAndersonArt <122129049+MilesAndersonArt@users.noreply.github.com>
2023-06-14 00:13:08 -0700

    Tweaked Button.js for past commits

--- src/Button.js
+++ src/Button.js
@@ -11,7 +11,7 @@ class Button {
         if(style){
             button.setStyle(style)
         } else {
-            button.setStyle({ backgroundColor: '#F3B141', fill: '#000000' })
+            button.setStyle({ backgroundColor: '#F3B141', fill: '#FFFFF' })
         }
     }
 }
